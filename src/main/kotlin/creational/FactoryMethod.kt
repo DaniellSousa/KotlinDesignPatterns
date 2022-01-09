b57d2bb422352532633cf28b9f3e29088d40b0fe
DaniellSousa <daniell.monteirosousa@gmail.com>
2022-01-09 16:47:52 -0300

    Factory Method Design Pattern example and its Test

--- src/main/kotlin/creational/FactoryMethod.kt
+++ src/main/kotlin/creational/FactoryMethod.kt
@@ -1,4 +1,36 @@
 package creational
 
-class FactoryMethod {
+/*
+* Design Pattern Factory Method: Para facilitar o uso e a chamada de métodos para algo específico, que não pode ser acessado
+*                               diretamente de um único lugar(ou única classe).
+*                               Exemplo: Classe País que tem uma Moeda específica e a implementação da classe moeda
+*                               depende do tipo de país, mesmo sem uma classe extender da outra. Uma classe é criada
+*                               "no meio" dessa duas classes para haver a ligação entre as mesmas.
+*
+* Design Pattern Factory Method: To make it easier to use and call methods for something specific that cannot be accessed
+*                               directly from a single place(or single class).
+*                               Example: Country Class which has a specific Currency and the implementation of the Currency class
+*                               depends on the type of country, even without an extender class of the other. A class is created
+*                               "in the middle" of these two classes so that there is a connection between them.
+*
+* */
+
+sealed class Country {
+    object Canada: Country()
+}
+
+object Spain: Country()
+class Greece: Country()
+class USA: Country()
+
+class Currency(val code: String)
+
+object CurrencyFactory {
+    fun currencyForFactory(country: Country): Currency =
+        when (country) {
+            is Spain -> Currency("EURO")
+            is Greece -> Currency("EURO")
+            is USA -> Currency("DOLAR")
+            is Country.Canada -> Currency("DOLAR CANADA")
+        }
 }
--- src/test/kotlin/creational/FactoryMethodTest.kt
+++ src/test/kotlin/creational/FactoryMethodTest.kt
@@ -1,2 +1,23 @@
 package creational
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class FactoryMethodTest {
+
+    @Test
+    fun currencyTest() {
+        val greekCurrency = CurrencyFactory.currencyForFactory(Greece()).code
+        println("Greek currency: $greekCurrency")
+
+        val usaCurrenncy = CurrencyFactory.currencyForFactory(USA()).code
+        println("USA currency: $usaCurrenncy")
+
+        val canadaCurrency = CurrencyFactory.currencyForFactory(Country.Canada).code
+        println("Canada currency: $canadaCurrency")
+
+        Assertions.assertEquals("EURO", greekCurrency)
+        Assertions.assertEquals("DOLAR", usaCurrenncy)
+        Assertions.assertEquals("DOLAR CANADA", canadaCurrency)
+    }
+}
