c958631fbe5c8b635033050f47b27f53ef72f775
DaniellSousa <daniell.monteirosousa@gmail.com>
2022-01-12 20:47:24 -0300

    :+1: Design Pattern Facade and its test

--- src/main/kotlin/structural/Facade.kt
+++ src/main/kotlin/structural/Facade.kt
@@ -1,2 +1,42 @@
 package structural
 
+/*
+* Design Pattern Facade: Técnica usada para simplficar problemas complexos utilizando "interfaces de simplificação".
+*
+* Design Pattern Facade: Technique used to simplify complex problems using "simplification interfaces".
+*
+* */
+
+// Pequeno exemplo de classe/problema complexo:
+class ComplexSystemStore(private val filePath: String) {
+    private val cache: HashMap<String, String>
+
+    init {
+        println("Reading data from the file: $filePath")
+        cache = HashMap()
+    }
+
+    fun store(key: String, value: String) {
+        cache[key] = value
+    }
+
+    fun read(key: String) = cache[key] ?: ""
+
+    fun commit() = println("Storing cached data to file $filePath")
+}
+//
+
+data class User(val login: String)
+
+// Facade:
+class UserRepository {
+    private val systemPreferences = ComplexSystemStore("/data/default.prefs")
+
+    fun save(user: User) {
+        systemPreferences.store("USER_KEY", user.login)
+        systemPreferences.commit()
+    }
+
+    fun findFirst(): User = User(systemPreferences.read("USER_KEY"))
+}
+//
--- src/test/kotlin/structural/FacadeTest.kt
+++ src/test/kotlin/structural/FacadeTest.kt
@@ -1,2 +1,17 @@
 package structural
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class FacadeTest {
+    @Test
+    fun testFacade() {
+        val userRepository = UserRepository()
+        val user = User("daniel")
+        userRepository.save(user)
+
+        val retrievedUser = userRepository.findFirst()
+
+        Assertions.assertEquals("daniel", retrievedUser.login)
+    }
+}
